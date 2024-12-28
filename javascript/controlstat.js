e88869af6cfbd21428870c481340af9f19696269
Madhuvanthi M <madhuvanthimatheswaran@gmail.com>
2024-12-28 11:31:11 +0530

    adding javascript

---
+++ javascript/controlstat.js
@@ -0,0 +1,7 @@
+a=20
+if(a==20){
+    console.log(a,"is even");
+}
+else{
+    console.log(a,"is odd");
+}
---
+++ javascript/operators.js
@@ -0,0 +1,34 @@
+// 1. Assignment operator
+let a = 10;
+console.log(a); 
+
+// 2. Unary operator
+a = 20;
+a++; 
+console.log(a); 
+
+// 3. Arithmetic operator
+a = 20;
+let b = 50;
+console.log(a + b);
+
+// 4. Relational operator
+a = 20;
+b = 30;
+console.log(a < b); 
+
+// 5. Logical operator
+a = 20;
+b = 30;
+console.log(a && b);
+console.log(a || b);
+
+// 6. Bitwise operator
+a = 30;
+b = 40;
+console.log(a & b); 
+
+// 7. Ternary operator
+const age = 11;
+const ba = age >= 18 ? "Major" : "Minor"; 
+console.log(ba);
