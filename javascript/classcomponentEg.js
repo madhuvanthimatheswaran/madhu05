7d59817a459371aec8e5de97a49093902fed2a7c
Madhuvanthi M <madhuvanthimatheswaran@gmail.com>
2024-12-29 21:58:24 +0530

    add

---
+++ javascript/classcomponentEg.js
@@ -0,0 +1,9 @@
+class ClassEg{
+    a = 10
+    methodEg = () =>{
+        console.log("Welcome to OOP'S");
+    }
+}
+let b = new ClassEg()
+console.log(b.a);
+console.log(b.methodEg())
---
+++ mern/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ClassCompEg from 
+
+function App() {
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
